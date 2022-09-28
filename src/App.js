40940dde14ea4768cd575584bc130fee8c7f79aa
Tunay Bektas <what05@web.de>
2022-09-28 12:31:33 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 import ActiveToggle from "./components/ActiveToggle/ActiveToggle";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 import ColorPage from "./components/ColorPage/ColorPage";
 import Hungry from "./components/Hungry/Hungry";
-import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 
 function App() {
   return (
     <div className="App">
-      {/* <ColorPage /> */}
-      {/* <Hungry /> */}
-      {/* <ActiveToggle /> */}
+      <ActiveToggle />
       <ShoppingCart />
     </div>
   );
